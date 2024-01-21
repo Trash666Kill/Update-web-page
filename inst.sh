28dd3fdd941b5cf45d963efb6e11a1c1b3956c6e
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-01-20 23:09:17 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -13,6 +13,13 @@ echo "1"
 apt install apache2 -y
 echo "**CREATING DIRECTORIES**"
 mkdir -pv /var/www/html/files
+chmod 755 -R /var/www/html/files
 echo "**SETTING UP BASE**"
 rm -v /etc/apache2/sites-available/000-default.conf
 cp -v 000-default.conf /etc/apache2/sites-available
+cp -v index.html script.js upload.php style.css /var/www/html
+echo "**ENTER A PASSWORD TO ACCESS THE SENT FILES**"
+htpasswd -c /etc/apache2/.htpasswd emperor
+systemctl restart apache2
+#End
+echo "**END**"
